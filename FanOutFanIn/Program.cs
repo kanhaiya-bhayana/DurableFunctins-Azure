ec4efebcc180bdbd240f8ad85091b5b3a9f3cd3b
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2024-09-21 15:21:10 +0530

    Serilog

--- FanOutFanIn/Program.cs
+++ FanOutFanIn/Program.cs
@@ -1,29 +1,41 @@
+using RandomNameGeneratorLibrary;
+using Serilog;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ApplicationInsights.Extensibility;
 using FanOutFanIn.Service.Interfaces;
 using FanOutFanIn.Service.Services;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using RandomNameGeneratorLibrary;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureAppConfiguration(config =>{
-        config.
-            AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddEnvironmentVariables()
-            .Build();
-    })
-    .ConfigureServices(services =>
+    .ConfigureAppConfiguration((context, config) =>
     {
+        config
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+    })
+    .ConfigureServices((context, services) =>
+    {
+        var config = context.Configuration;
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(config)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.ApplicationInsights(TelemetryConfiguration.CreateDefault(), TelemetryConverter.Traces)
+            .CreateLogger();
+
+        services.AddLogging(lb => lb.AddSerilog(Log.Logger, true));
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
         services.AddTransient<ICalculateProbabilityService, CalculateProbabilityService>();
         services.AddTransient<ISentimentReportGenerator, SentimentReportGenerator>();
         services.AddTransient<IAzureStorageProvider, AzureStorageProvider>();
         services.AddTransient<IPersonNameGenerator, PersonNameGenerator>();
-        services.AddLogging();
+
         services.Configure<KestrelServerOptions>(options =>
         {
             options.AllowSynchronousIO = true;
